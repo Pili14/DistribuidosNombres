f272aafafe10c30899a4f2b22d01b0729195bff3
Pili14 <maria.gomez18@uptc.edu.co>
2023-08-28 19:41:34 -0500

    prueba1

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const fs = require("fs")
+const server = express()
+
+server.get("/random_name", (req, res) => {
+  let person = getRandomPerson()
+  res.send({"author" : "Pilar", "nombre" : person.nombre, "genero" : person.genero})
+})
+
+server.listen(3000, () => {
+  console.log("Servidor activo en el puerto 3000")
+})
+
+function getRandomPerson(){
+  let persons = JSON.parse(fs.readFileSync("./persons.txt", "utf-8"))
+  persons = persons.persons
+  let random = Math.floor(Math.random() * (persons.length))
+  return persons[random]
+}
+
+server.get("/random_lastname", (req, res) => {
+  let person = getRandomPerson()
+  res.send({"author" : "Pilar", "apellido" : person.apellido, "genero" : person.genero})
+})
