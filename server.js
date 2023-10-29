73eb38563a5387161baff555394b81da1836fd54
Pili14 <maria.gomez18@uptc.edu.co>
2023-10-29 13:11:51 -0500

    se agrega host a la respuesta

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-const express = require("express")
-const fs = require("fs")
-const server = express()
+const express = require("express");
+const fs = require("fs");
+const server = express();
 
 server.get("/random_name", (req, res) => {
-  let person = getRandomPerson()
-  res.send({"author" : "Pilar Gomez", "nombre" : person.nombre, "genero" : person.genero})
-})
+  let person = getRandomPerson();
+  res.send({
+    author: "Pilar Gomez",
+    nombre: person.nombre,
+    genero: person.genero,
+    host: req.headers.host,
+  });
+});
 
 server.listen(3000, () => {
-  console.log("Servidor activo en el puerto 3000")
-})
+  console.log("Servidor activo en el puerto 3000");
+});
 
-function getRandomPerson(){
-  let persons = JSON.parse(fs.readFileSync("./data/persons.txt", "utf-8"))
-  persons = persons.persons
-  let random = Math.floor(Math.random() * (persons.length))
-  return persons[random]
+function getRandomPerson() {
+  let persons = JSON.parse(fs.readFileSync("./data/persons.txt", "utf-8"));
+  persons = persons.persons;
+  let random = Math.floor(Math.random() * persons.length);
+  return persons[random];
 }
 
 server.get("/random_lastname", (req, res) => {
-  let person = getRandomPerson()
-  res.send({"author" : "Pilar Gomez", "apellido" : person.apellido, "genero" : person.genero})
-})
+  let person = getRandomPerson();
+  res.send({
+    author: "Pilar Gomez",
+    apellido: person.apellido,
+    genero: person.genero,
+    host: req.headers.host,
+  });
+});
